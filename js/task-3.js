c5184abb25c439313b52d13450f5db99fc30b5c4
Khamala <elektrikamegawolt@gmail.com>
2024-02-29 15:44:39 +0100

    task2+3

---
+++ js/task-3.js
@@ -0,0 +1,35 @@
+
+class StringBuilder {
+    #value = '';
+
+    constructor(initialValue) {
+        this.#value = initialValue;
+    }
+    getValue() {
+        return this.#value;
+    }
+    padEnd(str){
+        this.#value = this.#value + str;
+    }
+    padStart(str) {
+        this.#value = str + this.#value;
+    }
+    padBoth(str) {
+        this.#value = str + this.#value + str;
+    }
+}   
+
+const builder = new StringBuilder(".");
+
+console.log(builder);
+
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
+
+//У результаті виклику new StringBuilder(".") значення приватної змінної builder — це об'єкт
+// Об'єкт builder не містить публічну властивість value
