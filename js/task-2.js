701232cdf4153b21dca3a6f81717eefa50320511
Khamala <elektrikamegawolt@gmail.com>
2024-03-01 13:13:19 +0100

    update task2

--- js/task-2.js
+++ js/task-2.js
@@ -14,7 +14,13 @@ class Storage {
   }
 
   removeItem(itemToRemove) {
+
     const ind = this.#items.indexOf(itemToRemove);
+
+    if (ind === -1) {
+      return;
+    }
+
     this.#items.splice(ind, 1);
   }
 }
@@ -25,3 +31,6 @@ storage.addItem("Droid");
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
 storage.removeItem("Prolonger");
 console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Pro");
+console.log(storage.getItems()); 
